92837d404235ea7d80a50e9ea7408340b1271a13
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-04-23 00:37:10 -0300

    chore: retrieve authentication state

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "../services/api";
 import Router from 'next/router'
-import { setCookie } from "nookies";
+import { parseCookies, setCookie } from "nookies";
 
 type User = {
   email: string;
@@ -30,6 +30,18 @@ export function AuthProvider({children}: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { 'next-authorization-authentication.token': token } = parseCookies();
+
+    if (token) {
+      api.get("/me").then(response => {
+        const { email, permissions, roles } = response.data;
+
+        setUser({ email, permissions, roles });
+      });
+    }
+
+  }, [])
 
   async function signIn({email, password}: SignInCredentials) {
     try {
@@ -54,6 +66,10 @@ export function AuthProvider({children}: AuthProviderProps) {
         permissions,
         roles
       })
+
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      // api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
       Router.push("/dashboard")
     } catch(err) {
